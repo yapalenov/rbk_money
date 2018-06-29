a4aefb0364bef6ceb178d17dceb85b582a388b54
yapalenov <palenov.yaroslav@gmail.com>
2018-06-29 17:48:17 +0400

    v_0.0

---
+++ index.js
@@ -0,0 +1,114 @@
+const Altruist = require('./models.js').Altruist;
+const Kidala = require('./models.js').Kidala;
+const Hitrets = require('./models.js').Hitrets;
+const Random = require('./models.js').Random;
+const Zlopamyatny = require('./models.js').Zlopamyatny;
+const Ushly = require('./models.js').Ushly;
+
+const dealer_types = ['Altruist', 'Kidala', 'Hitrets', 'Random', 'Zlopamyatny', 'Ushly'];
+const dealers_count = 60;
+
+let dealers = [];
+let dealers_not_ready = [];
+let types_counter = {}
+create_dealers();
+update_types_counter();
+
+while (!check_finish()) {
+  update_types_counter();
+  while(dealers_not_ready.length > 0) {
+    make_deal();
+  }
+  dealers.sort(down_sort);
+  update_dealers()
+  types_counter_incr();
+  iteration_result();
+}
+
+
+
+//----------------------------------//
+function create_dealers() {
+  for (let i = 0; i < dealer_types.length; i++) {
+    for (let j = 0; j < Math.floor(dealers_count / dealer_types.length); j++) {
+      dealers_not_ready.push({id: dealers.length});
+      dealers.push(create_dealer(dealer_types[i], dealers.length, dealers_count));
+    }
+  }
+}
+function create_dealer(name, id) {
+  if (name == "Altruist") return new Altruist(id, dealers_count);
+  if (name == "Kidala") return new Kidala(id, dealers_count);
+  if (name == "Hitrets") return new Hitrets(id, dealers_count);
+  if (name == "Random") return new Random(id, dealers_count);
+  if (name == "Zlopamyatny") return new Zlopamyatny(id, dealers_count);
+  if (name == "Ushly") return new Ushly(id, dealers_count);
+}
+function make_deal(iter) {
+  let dealer_id = dealers_not_ready[Math.floor(Math.random() * dealers_not_ready.length)].id;
+  let dealer = dealers.find((e) => { return e.id == dealer_id; });
+  let oponent_id = dealer.get_oponent_id;
+  let oponent = dealers.find((e) => { return e.id == oponent_id; });
+  dealer.deal(oponent);
+
+  if (dealer.is_ready) {
+    dealers_not_ready.forEach((item, i)=>{
+      if (item.id == dealer.id) dealers_not_ready.splice(i, 1);
+    })
+  }
+  if (oponent.is_ready) {
+    dealers_not_ready.forEach((item, i)=>{
+      if (item.id == oponent.id) dealers_not_ready.splice(i, 1);
+    })
+  }
+}
+function down_sort(a, b){
+  if (a.coins < b.coins) {
+    return 1
+  }
+
+  if (a.coins > b.coins) {
+    return -1
+  }
+
+  return 0;
+}
+function update_dealers() {
+  for (var i = 0; i < 12; i++) {
+    dealers.splice(-i - 1, 1, create_dealer(dealers[i]._name, dealers[59-i]._id));
+  }
+  dealers.forEach((item, i)=>{
+    item.reset(dealers_count);
+    dealers_not_ready.push({id: i});
+   })
+}
+function types_counter_incr() {
+  dealers.forEach((item, i)=> {
+    if (item._name == 'Altruist') {types_counter.Altruist++};
+    if (item._name == 'Kidala') {types_counter.Kidala++};
+    if (item._name == 'Hitrets') {types_counter.Hitrets++};
+    if (item._name == 'Random') {types_counter.Random++};
+    if (item._name == 'Zlopamyatny') {types_counter.Zlopamyatny++};
+    if (item._name == 'Ushly') {types_counter.Ushly++};
+  })
+}
+function update_types_counter() {
+  dealer_types.forEach((item, i)=>{
+    types_counter[item] = 0;
+  })
+}
+function iteration_result() {
+  console.log('--------------');
+  console.log('Altruist: ' + types_counter.Altruist);
+  console.log('Kidala: ' + types_counter.Kidala);
+  console.log('Hitrets: ' + types_counter.Hitrets);
+  console.log('Random: ' + types_counter.Random);
+  console.log('Zlopamyatny: ' + types_counter.Zlopamyatny);
+  console.log('Ushly: ' + types_counter.Ushly);
+}
+function check_finish() {
+  for (let prop in types_counter) {
+    if (types_counter[prop] == 60) return true;
+  }
+  return false;
+}
---
+++ models.js
@@ -0,0 +1,180 @@
+class Dealer {
+  constructor (id, name, dealers_count) {
+    this._id = id;
+    this._name = name;
+    this._coins = 0;
+    this._oponent_last_step = true;
+    this._step_number = 0;
+    this._oponent_lied = false;
+    this._oponents_steps_counter = [];
+    this._is_ready_count = 0;
+    this._strategy = [true, false, true, true];
+    this._kidala_strategy = '';
+    let set_steps = () => {
+      for (let i = 0; i < dealers_count; i++) {
+        this._oponents_steps_counter.push({id: i, steps: 0, is_ready: false})
+      }
+    }
+    set_steps();
+  }
+
+  reset(dealers_count) {
+    let set_steps = () => {
+      for (let i = 0; i < dealers_count; i++) {
+        this._oponents_steps_counter.push({id: i, steps: 0, is_ready: false})
+      }
+    }
+    this._coins = 0;
+    this._oponent_last_step = true;
+    this._step_number = 0;
+    this._oponent_lied = false;
+    this._oponents_steps_counter = [];
+    this._is_ready_count = 0;
+    this._strategy = [true, false, true, true];
+    this._kidala_strategy = '';
+    set_steps();
+  }
+
+  //Вводим 5% вероятность ошибки
+  step_now() {
+    let rand = Math.floor(Math.random()*20);
+    let five_error =  rand == 19;
+    return five_error ? !this.calculate_step() : this.calculate_step();
+  }
+  //Инкремент шага
+  step_incr() {
+    this._step_number = this._step_number + 1
+  }
+
+  set oponents_steps_counter(id) {
+    this._oponents_steps_counter.forEach((item, i)=> {
+      if(item.id == id) {
+          item.steps++;
+          if (item.steps > 4 && !item.is_ready) {this._is_ready_count++; item.is_ready = true}
+          if (item.steps > 9) this._oponents_steps_counter.splice(i, 1);
+      }
+    });
+
+  }
+
+
+  set oponent_last_step(step) {
+    this._oponent_last_step = step;
+    if (!step) this._oponent_lied = true;
+  }
+
+  get oponent_last_step() {
+    return this._oponent_last_step;
+  }
+
+  set coins(sum) {
+    this._coins = this._coins + sum;
+  }
+
+  get coins() {
+    return this._coins;
+  }
+
+  set id(id) {
+    this._id = id;
+  }
+
+  get id() {
+    return this._id;
+  }
+
+  get get_oponent_id () {
+    return this._oponents_steps_counter[Math.floor(Math.random() * this._oponents_steps_counter.length)].id;
+  }
+
+  get is_ready() {
+      return this._is_ready_count == 60 ? true : false;
+  }
+
+  deal(dealer) {
+    let self_step = this.step_now();
+    let oponent_step = dealer.step_now();
+    if (self_step && oponent_step) {
+      this.coins = 4;
+      dealer.coins = 4;
+    } else if (self_step && !oponent_step) {
+      this.coins = 1;
+      dealer.coins = 5;
+    } else if (!self_step && oponent_step) {
+      this.coins = 5;
+      dealer.coins = 1;
+    } else {
+      this.coins = 2;
+      dealer.coins = 2;
+    }
+    this.oponent_last_step = oponent_step;
+    dealer.oponent_last_step = self_step;
+    this.step_incr();
+    dealer.step_incr();
+    this.oponents_steps_counter = dealer.id;
+    dealer.oponents_steps_counter = this.id;
+  }
+}
+
+module.exports.Altruist = class Altruist extends Dealer {
+  constructor(id, dealers_count) {
+    super(id, "Altruist", dealers_count);
+  }
+  calculate_step() {
+    return true;
+  }
+}
+
+module.exports.Kidala = class Kidala extends Dealer {
+  constructor(id, dealers_count) {
+    super(id, "Kidala", dealers_count);
+  }
+  calculate_step() {
+    return false;
+  }
+}
+
+module.exports.Hitrets = class Hitrets extends Dealer {
+  constructor(id, dealers_count) {
+    super(id, "Hitrets", dealers_count);
+  }
+  calculate_step() {
+    return this._oponent_last_step;
+  }
+}
+
+module.exports.Random = class Random extends Dealer {
+  constructor(id, dealers_count) {
+    super(id, "Random", dealers_count);
+  }
+  calculate_step() {
+    return Math.floor(Math.random()*2) == 1;
+  }
+}
+
+module.exports.Zlopamyatny = class Zlopamyatny extends Dealer {
+  constructor(id, dealers_count) {
+    super(id, "Zlopamyatny", dealers_count);
+  }
+  calculate_step() {
+    return this._oponent_lied  ? false : true;
+  }
+}
+
+module.exports.Ushly = class Ushly extends Dealer {
+  constructor(id, dealers_count) {
+    super(id, "Ushly", dealers_count);
+  }
+  calculate_step() {
+    if (this._step_number < 4) {
+      return this._strategy[this._step_number]
+    } else if (this._step_number == 4) {
+      this._kidala_strategy = this._oponent_lied ? true : false;
+      return this._kidala_strategy ? false : this._oponent_last_step;
+    } else {
+      return this._kidala_strategy ? false : this._oponent_last_step;
+    }
+  }
+}
+
+module.exports.create_dealer = Dealer.create_dealer;
